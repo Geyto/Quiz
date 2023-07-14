0485f3cd90ff6f269cc5f6b5816304f91b5442e4
Roman Geyto <vrgeyto@gmail.com>
2023-07-14 14:22:48 +0300

    take data new page

--- script/result.js
+++ script/result.js
@@ -4,7 +4,17 @@
         init(){
             const url = new URL(location.href);
             document.getElementById('result__score').innerText = url.searchParams.get('score') +'/' + url.searchParams.get('total');
+            this.linkResult = document.getElementById('link');
+            console.log(this.linkResult)
+            this.linkResult.addEventListener('click', this.findAnswer)
         },
+        findAnswer(){
+            const url = new URL(location.href);
+            const id = url.searchParams.get('id');
+            const score = url.searchParams.get('score');
+            const total = url.searchParams.get('total');
+            location.href = 'list.html?score=' + score + '&total=' + total + '&id=' +id;
+        }
     }
     Result.init();
 })();
